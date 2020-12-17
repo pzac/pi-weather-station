ea83da976e4267f4273f72027ed4e6068450aaa7
Paolo Zaccagnini <paolo@pzac.net>
2020-12-17 13:34:03 +0100

    Read latest data from db and show in HTML page

---
+++ app/README.md
@@ -0,0 +1,3 @@
+Start the app in development mode and code reload with
+
+    FLASK_APP=web.py FLASK_ENV=development flask run
--- app/web.py
+++ app/web.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask import render_template
+import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="./templates")
 
 @app.route("/")
 def index():
-    return "HELLO"
+    try:
+        conn = sqlite3.connect("data.db")
+        cursor = conn.cursor()
+        query = "SELECT * FROM data ORDER BY time DESC LIMIT 1"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        return render_template("index.html", data=rows[0])
+    except sqlite3.Error as error:
+        return error
+    finally:
+        if (conn):
+            conn.close()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')

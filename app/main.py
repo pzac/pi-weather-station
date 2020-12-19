44c8f9a53581fff4764e0d38bd6e05031da1c411
Paolo Zaccagnini <paolo@pzac.net>
2020-12-19 22:35:17 +0100

    Let the loop crash

--- app/main.py
+++ app/main.py
@@ -7,34 +7,26 @@ interval = 60.0
 hub = SensorHub(None)
 debug = False
 
+connection = sqlite3.connect("data.db")
+
 sql = "INSERT INTO data(ext_temp, int_temp, humidity, motion, brightness, bar_temp, pressure) VALUES(?, ?, ?, ?, ?, ?, ?)"
 
-
 while True:
-    try:
-        connection = sqlite3.connect("data.db")
+    ext_temp   = hub.get_off_board_temperature()
+    humidity   = hub.get_humidity()
+    int_temp   = hub.get_temperature()
+    motion     = hub.is_motion_detected()
+    brightness = hub.get_brightness()
+    bar_temp   = hub.get_barometer_temperature()
+    pressure   = hub.get_barometer_pressure()
 
-        ext_temp   = hub.get_off_board_temperature()
-        humidity   = hub.get_humidity()
-        int_temp   = hub.get_temperature()
-        motion     = hub.is_motion_detected()
-        brightness = hub.get_brightness()
-        bar_temp   = hub.get_barometer_temperature()
-        pressure   = hub.get_barometer_pressure()
+    if debug:
+        localtime = time.localtime()
+        t = time.strftime("%I:%M:%S %p", localtime)
+        print(t + "\t"+ "Int Temp: " + str(int_temp) + "C Ext Temp: " + str(ext_temp) + "C")
 
-        if debug:
-            localtime = time.localtime()
-            t = time.strftime("%I:%M:%S %p", localtime)
-            print(t + "\t"+ "Int Temp: " + str(int_temp) + "C Ext Temp: " + str(ext_temp) + "C")
+    with closing(connection.cursor()) as cursor:
+        cursor.execute(sql, (ext_temp, int_temp, humidity, motion, brightness, bar_temp, pressure))
+        connection.commit()
 
-        with closing(connection.cursor()) as cursor:
-            cursor.execute(sql, (ext_temp, int_temp, humidity, motion, brightness, bar_temp, pressure))
-            connection.commit()
-
-        time.sleep(interval)
-    except sqlite3.Error as error:
-        print("Failed to insert data", error)
-    finally:
-        if (connection):
-            connection.close()
-            print("The SQLite connection is closed")
+    time.sleep(interval)

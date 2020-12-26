38e3654959cb57d5ce01c2866eae500cfb466273
Paolo Zaccagnini <paolo@pzac.net>
2020-12-26 11:54:54 +0100

    Make sensor loop more robust

--- app/main.py
+++ app/main.py
@@ -14,24 +14,20 @@ sql = "INSERT INTO data(ext_temp, int_temp, humidity, motion, brightness, bar_te
 hub = PiSensor()
 
 while True:
-    try:
-        ext_temp   = hub.ext_temp()
-        humidity   = hub.humidity()
-        int_temp   = hub.int_temp()
-        motion     = hub.motion()
-        brightness = hub.brightness()
-        bar_temp   = hub.bar_temp()
-        pressure   = hub.pressure()
+    ext_temp   = hub.ext_temp()
+    humidity   = hub.humidity()
+    int_temp   = hub.int_temp()
+    motion     = hub.motion()
+    brightness = hub.brightness()
+    bar_temp   = hub.bar_temp()
+    pressure   = hub.pressure()
 
-        if debug:
-            localtime = time.localtime()
-            t = time.strftime("%I:%M:%S %p", localtime)
-            print(t + "\t"+ "Int Temp: " + str(int_temp) + "C Ext Temp: " + str(ext_temp) + "C")
+    if debug:
+        localtime = time.localtime()
+        t = time.strftime("%I:%M:%S %p", localtime)
+        print(t + "\t"+ "Int Temp: " + str(int_temp) + "C Ext Temp: " + str(ext_temp) + "C")
 
-        with closing(connection.cursor()) as cursor:
-            cursor.execute(sql, (ext_temp, int_temp, humidity, motion, brightness, bar_temp, pressure))
-            connection.commit()
-        time.sleep(interval)
-    except IOError:
-        print("Error reading from sensor")
-
+    with closing(connection.cursor()) as cursor:
+        cursor.execute(sql, (ext_temp, int_temp, humidity, motion, brightness, bar_temp, pressure))
+        connection.commit()
+    time.sleep(interval)
--- app/pi_sensor.py
+++ app/pi_sensor.py
@@ -5,23 +5,49 @@ class PiSensor:
         self.hub = SensorHub(None)
 
     def ext_temp(self):
-        return self.hub.get_off_board_temperature()
+        try:
+            return self.safe_value(self.hub.get_off_board_temperature())
+        except IOError:
+            return None
 
     def humidity(self):
-        return self.hub.get_humidity()
+        try:
+            return self.safe_value(self.hub.get_humidity())
+        except IOError:
+            return None
 
     def int_temp(self):
-        return self.hub.get_temperature()
+        try:
+            return self.safe_value(self.hub.get_temperature())
+        except IOError:
+            return None
 
     def motion(self):
-        return self.hub.is_motion_detected()
+        try:
+            return self.safe_value(self.hub.is_motion_detected())
+        except IOError:
+            return None
 
     def brightness(self):
-        return self.hub.get_brightness()
+        try:
+            return self.safe_value(self.hub.get_brightness())
+        except IOError:
+            return None
 
     def bar_temp(self):
-        return self.hub.get_barometer_temperature()
+        try:
+            return self.safe_value(self.hub.get_barometer_temperature())
+        except IOError:
+            return None
 
     def pressure(self):
-        return self.hub.get_barometer_pressure()
+        try:
+            return self.safe_value(self.hub.get_barometer_pressure())
+        except IOError:
+            return None
 
+    def safe_value(self, x):
+        if x == -1:
+            return None
+        else:
+            return x
